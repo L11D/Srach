78f2c5cf069c94d385f1ab7ad78bb14cea1cd11a
Archi <129053616+OG-UZOK@users.noreply.github.com>
2023-05-25 16:45:52 +0700

    Srcoll Colsole
    Добален скрол в консоль, нужно слить с основой

--- app/src/main/java/com/example/srach/MainActivity.kt
+++ app/src/main/java/com/example/srach/MainActivity.kt
@@ -1,12 +1,99 @@
 package com.example.srach
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.example.srach.databinding.ActivityGeneralScreenBinding
+import com.example.srach.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var  bindingClass : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
+    }
+
+    fun onClickStart(view: View){
+        val intentCreate = Intent(this, Console::class.java)
+        intentCreate.putExtra("action", "Hello World!\nIts working!\nYes!\nA\n" +
+                "Agfdjgdfogjklfghdlkgfkgsdkjgfdkgjsdfgjldjgkldfjgjgldjgldfjglfdjlgkdjlftgkjdlgjdlkgjdflgjdlfgjldfjgdjgldfjgldfjgldfjglkdfjgldjflgdjlkgjdolgjdflgjdolgjdfoljfgd\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A\n" +
+                "A")
+        //тут надо подумать что передавать
+        startActivity(intentCreate)
+    }
+
+    fun onClickMenu(view: View){
+        finish();
     }
 
 }
