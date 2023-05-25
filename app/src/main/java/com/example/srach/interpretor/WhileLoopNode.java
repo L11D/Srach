a514491020df1202fa4b76f74ce9fc9f5a0cca1f
VitoMilize <114663229+VitoMilize@users.noreply.github.com>
2023-05-25 22:35:49 +0700

    add for loop

--- app/src/main/java/com/example/srach/interpretor/WhileLoopNode.java
+++ app/src/main/java/com/example/srach/interpretor/WhileLoopNode.java
@@ -1,37 +1,21 @@
 package com.example.srach.interpretor;
 
-public class WhileLoopNode extends LogicNode {
+public class WhileLoopNode extends LoopNode {
     private BeginNode loopBegin;
-    private LogicNode completed;
-    private MathNode condition;
     WhileLoopNode() {
         loopBegin = null;
-        completed = null;
-        condition = null;
     }
 
     public void setLoopBegin(BeginNode loopBegin) {
         this.loopBegin = loopBegin;
     }
 
-    public void setCompleted(LogicNode completed) {
-        this.completed = completed;
-    }
-
-    public LogicNode getCompleted() {
-        return completed;
-    }
-
-    public void setCondition(MathNode condition) {
-        this.condition = condition;
-    }
-
     @Override
     public void work() {
-        if (condition.evaluate().type == DataType.BOOL)
-            if (Boolean.parseBoolean(condition.evaluate().value))
+        if (getCondition().evaluate().type == DataType.BOOL)
+            if (Boolean.parseBoolean(getCondition().evaluate().value))
                 setNext(loopBegin);
             else
-                setNext(completed);
+                setNext(getCompleted());
     }
 }
